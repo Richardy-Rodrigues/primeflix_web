be3dde601a0ee01809e4a0b788d2f0135832f0ed
Richardy Gabriel Rodrigues da Costa <richardyrodrigues519@gmail.com>
2025-04-28 17:16:24 -0300

    Implementado hook useNavigate

--- src/pages/Filme/index.js
+++ src/pages/Filme/index.js
@@ -1,11 +1,13 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import './filme.css';
 
 import api from '../../services/api';
 
 function Filme() {
     const { id } = useParams();
+    const navigate = useNavigate();
+
     const [filme, setFilme] = useState({});
     const [loading, setLoading] = useState(true);
 
@@ -23,6 +25,8 @@ function Filme() {
             })
             .catch(() => {
                 console.log("Filme não encontrado!");
+                navigate('/', { replace: true });
+                return;
             })
         }
         loadFilme()
